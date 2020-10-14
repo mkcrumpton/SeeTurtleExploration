e4c50b4aa73e099e1f3e8a2bc8aa54d7a789cfbe
Marie Crumpton <marie.k.cr@gmail.com>
2020-10-14 13:57:00 -0400

    Init

---
+++ Assets/Scripts/LightController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightController : MonoBehaviour
+{
+    public GameObject player;
+    private Light theLight;
+    private float positionZ;
+    private Color underwater;
+    // Start is called before the first frame update
+    void Start()
+    {
+        theLight = GetComponent<Light>();
+        underwater = new Color(0.2548f, 0.4697f, 0.6509f, 1.0f);
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        positionZ = player.transform.position.z;
+        if (positionZ >= 56)
+        {
+            theLight.color = underwater;
+            //Debug.Log(underwater.ToString());
+        }
+        
+    }
+}
